908af273fff22cee676f991de5da18bb3bfcde6a
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-03-08 09:45:06 +0100

    feat: add sorting by popularity and rating in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,14 +6,36 @@ const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 function HomePage() {
     const [movies, setMovies] = useState([]);
+    const [sortOption, setSortOption] = useState("popularity");
 
     useEffect(() => {
-        fetchPopularMovies().then(setMovies);
-    }, []);
+        fetchPopularMovies().then(data => {
+            sortMovies(data, sortOption);
+        });
+    }, [sortOption]);
+
+    const sortMovies = (movies, option) => {
+        const sortedMovies = [...movies].sort((a, b) => {
+            if (option === "rating") {
+                return b.vote_average - a.vote_average;
+            } else {
+                return b.popularity - a.popularity;
+            }
+        });
+        setMovies(sortedMovies);
+    };
 
     return (
         <div className="container">
             <h1>Popular Movies</h1>
+
+            <div style={{ marginBottom: "20px" }}>
+                <select value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+                    <option value="popularity">Sort by Popularity</option>
+                    <option value="rating">Sort by Rating</option>
+                </select>
+            </div>
+
             <div className="movies-grid">
                 {movies.length > 0 ? (
                     movies.map(movie => (

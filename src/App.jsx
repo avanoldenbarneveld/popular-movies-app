410feab13c7b7ae0f5ce8bdb44954288f9bf4540
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-03-10 13:41:10 +0100

    feat: add routing for favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MovieDetails from "./pages/MovieDetails";
+import Favorites from "./pages/Favorites";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/movie/:id" element={<MovieDetails />} />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </Router>
     );

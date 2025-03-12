796f16d6d0ae4a1f6ec9254d52fb1a63dc47516d
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-03-12 11:34:05 +0100

    chore: add Jest setup and TextEncoder polyfill

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+require('@testing-library/jest-dom');
+
+if (typeof TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}

ea06118ecaf39aeb44e046fbd0372d50312d50e0
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-03-12 11:33:55 +0100

    chore: configure Jest and Babel for testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest'
+    }
+  };
+  
